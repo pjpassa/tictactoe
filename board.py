e432814eb5b9bde1fca1ff52fc4042cb4de5c528
PJ Passalacqua <pjpassa@gmail.com>
2015-06-04 18:03:03 -0400

    made a text_state property

--- board.py
+++ board.py
@@ -1,6 +1,6 @@
 class Board:
     def __init__(self, text_state):
-        self.board = [[value for value in text] for text in text_state]
+        self.board = [[value for value in row] for row in text_state]
 
     def get_value(self, coordinates):
         return self.board[coordinates[0]][coordinates[1]]
@@ -15,3 +15,7 @@ class Board:
         col_start = 2*diag_number
         col_inc = -1**diag_number
         return [self.board[i][col_start + i*col_inc] for i in range(0, 2)]
+
+    @property
+    def text_state(self):
+        return [''.join(row) for row in self.board]
