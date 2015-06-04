0957a08c2d8dcec3cb47e574ae52da446b5fcdc8
PJ Passalacqua <pjpassa@gmail.com>
2015-06-04 18:11:43 -0400

    added victory chance detection

--- board.py
+++ board.py
@@ -14,8 +14,24 @@ class Board:
     def get_diag(self, diag_number):
         col_start = 2*diag_number
         col_inc = -1**diag_number
-        return [self.board[i][col_start + i*col_inc] for i in range(0, 2)]
+        return [self.board[i][col_start + i*col_inc] for i in range(2)]
 
     @property
     def text_state(self):
         return [''.join(row) for row in self.board]
+
+    def detect_victory_chance(self, set):
+        if set[0] == set[1] or set[0] == set[2]:
+            return set[0]
+        elif set[1] == set[2]:
+            return set[1]
+        return "_"
+
+    def row_victory_chance(self, row_number):
+        return(self.detect_victory_chance(self.board.get_row(row_number)))
+
+    def col_victory_chance(self, col_number):
+        return(self.detect_victory_chance(self.board.get_col(col_number)))
+
+    def diag_victory_chance(self, diag_number):
+        return(self.detect_victory_chance(self.board.get_diag(diag_number)))
