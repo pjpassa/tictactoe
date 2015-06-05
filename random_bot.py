46aeb4bc0f2d2f86da005881c0adf8a453b6759d
PJ Passalacqua <pjpassa@gmail.com>
2015-06-05 16:35:37 -0400

    created basic random bot

--- random_bot.py
+++ random_bot.py
@@ -1,2 +1,19 @@
-from board import Board
+import random
+import sys
+from board import Board, import_tic_tac_toe_board
 import board_evaluator
+
+
+def debug_print(information):
+    print("DEBUG: ", information, file=sys.stderr)
+
+board = Board(import_tic_tac_toe_board()[1])
+#debug_print(board.text_state)
+coords = (1, 1)
+while True:
+    coords = (random.randint(0, 2), random.randint(0, 2))
+    if board.get_value(coords) == "_":
+        break
+
+#debug_print("{} {}".format(coords[0], coords[1]))
+print('{} {}'.format(coords[0], coords[1]))
