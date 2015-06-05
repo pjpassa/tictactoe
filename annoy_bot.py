bf7d84e49d95e8a0e0d20f3f76bf2d31fc28a92a
PJ Passalacqua <pjpassa@gmail.com>
2015-06-05 17:01:52 -0400

    created annoy_bot

--- annoy_bot.py
+++ annoy_bot.py
@@ -1,12 +1,11 @@
 import random
 from board import Board, import_tic_tac_toe_board
+import board_evaluator as be
 
 player, text_board = import_tic_tac_toe_board()
 board = Board(text_board)
 coords = (1, 1)
-while True:
+while board.get_value(coords) != "_":
     coords = (random.randint(0, 2), random.randint(0, 2))
-    if board.get_value(coords) == "_":
-        break
 
 print('{} {}'.format(coords[0], coords[1]))
