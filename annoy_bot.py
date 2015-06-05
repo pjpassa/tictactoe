8875ee5c500b80e0de7957007436bc1f19e222d7
PJ Passalacqua <pjpassa@gmail.com>
2015-06-05 17:10:35 -0400

    finished annoy_bot

--- annoy_bot.py
+++ annoy_bot.py
@@ -4,8 +4,13 @@ import board_evaluator as be
 
 player, text_board = import_tic_tac_toe_board()
 board = Board(text_board)
+corners = [(0, 2), (0, 0), (2, 0), (2, 2)]
+random.shuffle(corners)
 coords = (1, 1)
 while board.get_value(coords) != "_":
-    coords = (random.randint(0, 2), random.randint(0, 2))
+    if corners:
+        coords = corners.pop()
+    else:
+        coords = (random.randint(0, 2), random.randint(0, 2))
 
 print('{} {}'.format(coords[0], coords[1]))
